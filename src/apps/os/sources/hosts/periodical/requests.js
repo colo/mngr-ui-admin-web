b9f8c1fb8bd093aa5add07f481473aa97d6773ba
Luciano Bueno <glamero@gmail.com>
2020-06-04 11:10:48 -0300

    #0000 - bump

--- src/apps/os/sources/hosts/periodical/requests.js
+++ src/apps/os/sources/hosts/periodical/requests.js
@@ -156,7 +156,8 @@ const hosts_range_component = {
             } else if (
               /^os\.networkInterfaces\.eth.*\.bytes$/.test(path) ||
               /^os\.networkInterfaces\.eno.*\.bytes$/.test(path) ||
-              /^os\.networkInterfaces\.enp.*\.bytes$/.test(path)
+              /^os\.networkInterfaces\.enp.*\.bytes$/.test(path) ||
+              /^os\.networkInterfaces\.vlan.*\.bytes$/.test(path)
             ) {
               if (!hosts_data[host]['os.networkInterfaces.in']) hosts_data[host]['os.networkInterfaces.in'] = 0
               if (!hosts_data[host]['os.networkInterfaces.out']) hosts_data[host]['os.networkInterfaces.out'] = 0
